10ae6179186d4a7306803e439bfccd7086d2e504
mbrandol <mbrandol@uncc.edu>
2014-03-22 02:47:10 -0400

    Update to the BookPrototype
    The prototype now works, taking an ISBN as input and returning the
    Author and Title of the book

--- BookPrototype/src/com/bookprototype/MainActivity.java
+++ BookPrototype/src/com/bookprototype/MainActivity.java
@@ -77,69 +77,52 @@ public class MainActivity extends Activity {
     	    return content;
     	}
     
-    private String inputStreamToString(InputStream is) {
-        String s = "";
-        String line = "";
-        
-        // Wrap a BufferedReader around the InputStream
-        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-        
-        // Read response until the end
-        try {
-        	while ((line = rd.readLine()) != null) { s += line; }
-        } catch (Exception e) {
-        	Toast toast = Toast.makeText(getApplicationContext(), "Error loading string", Toast.LENGTH_SHORT);
-    		toast.show();
-        }
-        
-        // Return full string
-        return s;
-    }
-    
     public void buttonClick (View view) {
     	EditText isbnText = (EditText)findViewById(R.id.isbnText);
     	String isbnNum = isbnText.getText().toString();
-    	String url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbnNum + "&key=AIzaSyCTWEHd1S6rU1xHgMgHjUeZ5HGwdRaejaA";
+    	String url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbnNum;
     	//InputStream searchResult = getInputStreamFromUrl(url);
     	//String searchString = inputStreamToString(searchResult);
     	String searchString = "";
-    	new HttpGetter().execute(url, null, searchString);
-    	String TAG_ITEMS = "items";
-    	String TAG_SELFLINK = "selfLink";
-    	if (searchString != null) {
+    	try {
+    		searchString = new HttpGetter().execute(url, null, searchString).get();
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    		String TAG_ITEMS = "items";
+    		String TAG_VOLUMEINFO = "volumeInfo";
+    		String TAG_TITLE = "title";
+    		String TAG_AUTHORS = "authors";
+    	try {
+    		JSONObject returnedList = new JSONObject(searchString);
         	try {
-        		JSONObject returnedList = new JSONObject(searchString);
         		JSONArray bookList = returnedList.getJSONArray(TAG_ITEMS);
-        		String bookLink = bookList.getJSONObject(0).getString(TAG_SELFLINK);
-            	//InputStream bookResult = getInputStreamFromUrl(bookLink + "&key=AIzaSyCTWEHd1S6rU1xHgMgHjUeZ5HGwdRaejaA");
-            	//String bookString = inputStreamToString(bookResult);
-        		String bookString = "";
-        		try {
-        			bookString = new HttpGetter().execute(bookLink + "&key=AIzaSyCTWEHd1S6rU1xHgMgHjUeZ5HGwdRaejaA", null, "").get();
-        		} catch (Exception e) {
-        			e.printStackTrace();
-        		}
-            	String TAG_TITLE = "title";
-            	String TAG_AUTHORS = "authors";
             	try {
-            	    JSONObject book = new JSONObject(bookString);
-            	    String bookTitle = book.getString(TAG_TITLE);
-            	    EditText titleText = (EditText)findViewById(R.id.titleText);
-            	    titleText.setText(bookTitle);
-            	    String bookAuthor = book.getJSONArray(TAG_AUTHORS).getString(0);
-            	    EditText authorText = (EditText)findViewById(R.id.authorText);
-            	    authorText.setText(bookAuthor);
+            		JSONObject book = bookList.getJSONObject(0);
+                	try {
+                		JSONObject bookInfo = book.getJSONObject(TAG_VOLUMEINFO);
+                		String bookTitle = bookInfo.getString(TAG_TITLE);
+                		JSONArray authors = bookInfo.getJSONArray(TAG_AUTHORS);
+                		String bookAuthor = authors.getString(0);
+                		EditText titleText = (EditText)findViewById(R.id.titleText);
+                		titleText.setText(bookTitle);
+                		EditText authorText = (EditText)findViewById(R.id.authorText);
+                		authorText.setText(bookAuthor);
+                	} catch (JSONException e) {
+                		e.printStackTrace();
+                	}
             	} catch (JSONException e) {
-            		Toast toast = Toast.makeText(getApplicationContext(), "JSON error 2", Toast.LENGTH_SHORT);
-            		toast.show();
-            		e.printStackTrace();
+                	Toast toast = Toast.makeText(getApplicationContext(), "Get the book", Toast.LENGTH_LONG);
+                	toast.show();
             	}
         	} catch (JSONException e) {
-        		Toast toast = Toast.makeText(getApplicationContext(), searchString, Toast.LENGTH_LONG);
-        		toast.show();
-        		e.printStackTrace();
+            	Toast toast = Toast.makeText(getApplicationContext(), "Get the Array", Toast.LENGTH_LONG);
+            	toast.show();
         	}
+    	} catch (JSONException e) {
+        	Toast toast = Toast.makeText(getApplicationContext(), "Read the Object", Toast.LENGTH_LONG);
+        	toast.show();
     	}
+
     }
-    
 }
